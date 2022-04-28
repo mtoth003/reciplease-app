f26d869284ab437e807b0c8267e66c630eb986b9
Michael Toth <mtoth003@gmail.com>
2022-04-27 21:41:55 -0400

    adding styling to Popular component

---
+++ src/components/Popular.jsx
@@ -0,0 +1,96 @@
+import { useEffect, useState } from "react"
+import styled from 'styled-components'
+import { Splide, SplideSlide } from "@splidejs/react-splide"
+import '@splidejs/splide/dist/css/splide.min.css';
+
+function Popular() {
+
+  const [popular, setPopular] = useState([])
+
+  useEffect(() => {
+    getPopular()
+  }, [])
+
+  const getPopular = async () => {
+    const api = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9`)
+    const data = await api.json()
+    setPopular(data.recipes)
+  }
+
+  return (
+    <div>
+      <Wrapper>
+        <h3>Popular Picks</h3>
+        <Splide
+          options={{
+            perPage: 4,
+            arrows: true,
+            pagination: false,
+            drag: 'free',
+            gap: "5rem"
+          }}
+        >
+        {popular.map(({title, id, image}) => {
+          return (
+            <SplideSlide key={id}>
+              <Card>
+                <p>{title}</p>
+                <img src={image} alt={title} />
+                <Gradient />
+              </Card>
+            </SplideSlide>
+          )
+        })}
+        </Splide>
+      </Wrapper>
+    </div>
+  )
+}
+
+const Wrapper = styled.div`
+  margin: 4rem 0;
+`
+
+const Card = styled.div`
+  min-height: 25rem;
+  overflow: hidden;
+  position: relative;
+  border-radius: 2rem;
+
+
+  img {
+    position: absolute;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+    border-radius: 2rem;
+  }
+
+  p {
+    position: absolute;
+    z-index: 10;
+    left: 50%;
+    bottom: 0;
+    transform: translate(-50%, 0);
+    color: #fff;
+    width: 100%;
+    height: 40%;
+    text-align: center;
+    font-weight: 600;
+    font-size: 1rem;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+`
+
+const Gradient = styled.div`
+  z-index: 3;
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  background: linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.5));
+`
+
+export default Popular
